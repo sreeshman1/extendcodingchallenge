8396f126d0f0dda8834097df9bf54f8764358f11
Sreesh Manickababu <sreeshmanickababu@Sreeshs-MacBook-Air.local>
2022-09-28 09:02:25 -0700

    fix: linting

--- src/lambdas/handler/breeds-get.test.ts
+++ src/lambdas/handler/breeds-get.test.ts
@@ -6,35 +6,35 @@ const mockedFetch: jest.Mock = fetch as any
 jest.mock('node-fetch')
 
 const mockBreeds: Record<string, string[]> = {
-    'bulldog': ['boston', 'english', 'french'],
-    'poodle': []
+  bulldog: ['boston', 'english', 'french'],
+  poodle: [],
 }
 const mockPayload: Breeds = {
-    message: mockBreeds, status: "200"
+  message: mockBreeds,
+  status: '200',
 }
-const mockBreedList = ['boston bulldog', 'english bulldog', 'french bulldog', 'poodle'];
+const mockBreedList = ['boston bulldog', 'english bulldog', 'french bulldog', 'poodle']
 
 describe('breeds-get convertBreedstoBreedList', () => {
-    it('converts a record of breeds into a flat array', () => {
-        const breedList = convertBreedsIntoBreedList(mockBreeds);
-        expect(breedList).toEqual(mockBreedList);
-    });
+  it('converts a record of breeds into a flat array', () => {
+    const breedList = convertBreedsIntoBreedList(mockBreeds)
+    expect(breedList).toEqual(mockBreedList)
+  })
 })
 
 describe('breeds-get handler', () => {
-
-    it('returns the correct payload for a succesful fetch request', async () => {
-        mockedFetch.mockReturnValueOnce({
-            json: () => {
-                return mockPayload
-            },
-        })
-        const response = await handler()
-        expect(response).toMatchObject({ body: mockBreedList, statusCode: 200 });
+  it('returns the correct payload for a succesful fetch request', async () => {
+    mockedFetch.mockReturnValueOnce({
+      json: () => {
+        return mockPayload
+      },
     })
+    const response = await handler()
+    expect(response).toMatchObject({ body: mockBreedList, statusCode: 200 })
+  })
 
-    it("returns an error message for a failed fetch request", async () => {
-        mockedFetch.mockReturnValueOnce({});
-        await expect(handler()).resolves.toEqual({ message: "Something went wrong", statusCode: 500 });
-    })
+  it('returns an error message for a failed fetch request', async () => {
+    mockedFetch.mockReturnValueOnce({})
+    await expect(handler()).resolves.toEqual({ message: 'Something went wrong', statusCode: 500 })
+  })
 })
--- src/lambdas/handler/breeds-get.ts
+++ src/lambdas/handler/breeds-get.ts
@@ -2,45 +2,47 @@ import fetch from 'node-fetch'
 import { Response } from '../types'
 
 interface BreedsResponse extends Response {
-    body: String[]
+  body: string[]
 }
 
 interface ErrorResponse extends Response {
-    message: any
+  message: string
 }
 
 export interface Breeds {
-    message: Record<string, string[]>,
-    status: string
+  message: Record<string, string[]>
+  status: string
 }
 
 export const convertBreedsIntoBreedList = (breeds: Record<string, string[]>): string[] => {
-    const result: string[] = []
-    for (const [breed, subBreeds] of Object.entries(breeds)) {
-        if (subBreeds.length === 0) {
-            result.push(breed);
-        } else {
-            subBreeds.forEach((subBreed) => {
-                result.push(`${subBreed} ${breed}`);
-            });
-        }
+  const result: string[] = []
+  const breedEntries = Object.entries(breeds)
+  for (let i = 0; i < breedEntries.length; i += 1) {
+    const [breed, subBreeds] = breedEntries[i]
+    if (subBreeds.length === 0) {
+      result.push(breed)
+    } else {
+      subBreeds.forEach((subBreed) => {
+        result.push(`${subBreed} ${breed}`)
+      })
     }
-    return result;
+  }
+  return result
 }
 
 export async function handler(): Promise<BreedsResponse | ErrorResponse> {
-    try {
-        const res = await fetch('https://dog.ceo/api/breeds/list/all')
-        const payload: Breeds = await res.json()
-        const breedList = convertBreedsIntoBreedList(payload.message)
-        return {
-            statusCode: 200,
-            body: breedList,
-        }
-    } catch (err: unknown) {
-        return {
-            statusCode: 500,
-            message: 'Something went wrong',
-        }
+  try {
+    const res = await fetch('https://dog.ceo/api/breeds/list/all')
+    const payload: Breeds = await res.json()
+    const breedList = convertBreedsIntoBreedList(payload.message)
+    return {
+      statusCode: 200,
+      body: breedList,
     }
+  } catch (err: unknown) {
+    return {
+      statusCode: 500,
+      message: 'Something went wrong',
+    }
+  }
 }
